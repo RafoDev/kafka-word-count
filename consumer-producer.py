93a4203c7ad20227b6b09df6f975b2ea1a5b4f9d
RafoDev <rafael.david@ucsp.edu.pe>
2023-11-14 15:05:34 -0500

    feat(chat): same script but with threads.

--- consumer-producer.py
+++ consumer-producer.py
@@ -1,29 +1,42 @@
 from kafka import KafkaProducer, KafkaConsumer
-import time
+from threading import Thread
 from config import *
+import time
 
-producer = KafkaProducer(bootstrap_servers=server_addr+':9092')
+producer = KafkaProducer(bootstrap_servers=server_addr)
+
+# Configuración del consumidor
 consumer = KafkaConsumer(
     topic_name,
-    bootstrap_servers= server_addr + ':9092',
-    auto_offset_reset='earliest'
+    bootstrap_servers=server_addr,
+    auto_offset_reset='latest'
 )
 
-topic = topic_name
 
 def send_message(message):
-    producer.send(topic, message.encode('utf-8'))
+    producer.send(topic_name, message.encode('utf-8'))
 
 
-while True:
-    message = input("Mensaje: ")
-    if message == 'exit':
-        break
-    send_message(message)
-    print(f"Mensaje enviado: {message}")
+def receive_messages():
+    for message in consumer:
+        print(
+            f"\nMensaje recibido: {message.value.decode('utf-8')}\nTú: ", end='')
 
-for message in consumer:
-    msg = message.value.decode('utf-8')
-    print(f"Mensaje recibido: {msg}")
 
-consumer.close()
+receiver_thread = Thread(target=receive_messages)
+receiver_thread.daemon = True
+receiver_thread.start()
+
+print("Bienvenido al chat. Escribe 'exit' para salir.")
+
+try:
+    while True:
+        message = input("Tú: ")
+        if message.lower() == 'exit':
+            break
+        send_message(message)
+except KeyboardInterrupt:
+    pass
+finally:
+    producer.close()
+    consumer.close()
